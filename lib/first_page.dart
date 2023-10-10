6d1012afcf86eeb1ad9f03a018b817e7f4d4e2a5
ivanka1707 <vanka.angel17@gmail.com>
2023-10-10 14:39:08 +0700

    push ke 2

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -11,33 +11,64 @@ class FirstPage extends StatelessWidget {
         children: [
           Container(
             color: Colors.blue,
-            child: const Column(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisSize: MainAxisSize.max,
               children: [
-                Row(
+                const Row(
                   children: [
                     Padding(
-                      padding: EdgeInsets.all(25.0),
+                      padding: EdgeInsets.all(20.0),
                       child: CircleAvatar(
                         backgroundImage: AssetImage('/images/bang.jpeg'),
                         radius: 30,
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(0.0),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('Hey Robert Fox'),
-                          Text('Ready to learn grammar today?'),
+                          Text(
+                            'Hey Robert Fox',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            'Ready to learn grammar today?',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ],
                       ),
                     ),
                   ],
                 ),
-                Text('Hey, What would you like to learn today?'),
-                Text('Hey, What would you like to learn today?'),
+                const Padding(
+                  padding: EdgeInsets.only(left: 15, right: 100),
+                  child: Text(
+                    'Hey, What would you like to learn today?',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100.0),
+                        ),
+                        labelText: "Search here",
+                        suffixIcon: const Icon(Icons.search)),
+                  ),
+                ),
               ],
             ),
           ),
@@ -46,40 +77,86 @@ class FirstPage extends StatelessWidget {
             child: Column(
               children: [
                 Row(
-                  children: [
-                    Container(
-                      color: Colors.grey,
-                      width: 100,
-                      height: 150,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueGrey,
+                          ),
+                          child: const Column(
+                            children: [
+                              Text('Gambar'),
+                              Text('Reading'),
+                              Text('11 Taks'),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                    Container(
-                      color: Colors.blueGrey,
-                      width: 100,
-                      height: 150,
-                    ),
-                    Container(
-                      color: Colors.blue,
-                      width: 100,
-                      height: 150,
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueAccent,
+                          ),
+                          child: const Column(
+                            children: [
+                              Text('Gambar'),
+                              Text('Reading'),
+                              Text('11 Taks'),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
                 Row(
-                  children: [
-                    Container(
-                      color: Colors.blueGrey,
-                      width: 100,
-                      height: 150,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueGrey,
+                          ),
+                          child: const Column(
+                            children: [
+                              Text('Gambar'),
+                              Text('Reading'),
+                              Text('11 Taks'),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                    Container(
-                      color: Colors.blue,
-                      width: 100,
-                      height: 150,
-                    ),
-                    Container(
-                      color: Colors.grey,
-                      width: 100,
-                      height: 150,
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueAccent,
+                          ),
+                          child: const Column(
+                            children: [
+                              Text('Gambar'),
+                              Text('Reading'),
+                              Text('11 Taks'),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
